3ccf96cb435136ef4f3c67d610f92bd859d9896f
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2017-08-31 20:45:59 +0200

    small changes

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -10,8 +10,7 @@ export class FilmsComponent {
 
   public mobileScreen = true;
 
-  video = '/assets/gif/lense.mp4';
-  gif = '/assets/gif/lense.gif';
+  video = '/assets/gif/lense3.mp4';
 
   constructor(private winRef: WindowRefService) {
     if (winRef.nativeWindow.outerWidth > 768) {
--- src/app/gif/gif.component.ts
+++ src/app/gif/gif.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit, Input } from '@angular/core';
 export class GifComponent implements OnInit {
 
   @Input() video: string;
-  @Input() gif: string;
 
   constructor() { }
 

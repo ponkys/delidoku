ac33a23fdfd86dcc0a3e1f09ac8bbbdbcdb8bbed
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2017-10-09 23:16:40 +0200

    team update

--- src/app/team/team.ts
+++ src/app/team/team.ts
@@ -3,42 +3,48 @@ export const Team: any[] = [
         name: '<h2>Charlotte Symmons</h2>',
         role: 'Locations producer',
         // tslint:disable-next-line:max-line-length
-        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fcharlotte-symmons.png?alt=media&token=72dfbadd-6353-48af-8853-964b1f48b3f6'
+        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fcharlotte-symmons.png?alt=media&token=e3a3c11e-9c66-44b9-97d8-fc7d8ae94c00'
     },
     {
         name: '<h2>Veronika Gazner</h2>',
         role: 'Executive producer',
         // tslint:disable-next-line:max-line-length
-        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fveronika.png?alt=media&token=c656024d-f77e-4957-96b7-02f72f8fce3e'
+        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fveronika.png?alt=media&token=ff93584f-3669-454b-b9e8-2ee429ca96b1'
+    },
+    {
+        name: '<h2>Cecilia Bandeira</h2>',
+        role: 'Executive producer',
+        // tslint:disable-next-line:max-line-length
+        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fcecilia.png?alt=media&token=2a12128e-49d4-4605-bab7-7f4a230c7bf3'
     },
      {
         name: '<h2>Adam Breier</h2>',
         role: 'Programmer and artistic producer',
         // tslint:disable-next-line:max-line-length
-        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fadam.png?alt=media&token=9e8c139b-2dee-4fc0-a473-2cb0eaae94e5'
+        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fadam.png?alt=media&token=b335f809-e47b-480f-bef6-b1ebd65f8a05'
     },
     {
         name: '<h2>Priscilla Esteves</h2>',
         role: 'Communication manager',
         // tslint:disable-next-line:max-line-length
-        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fpriscilla.png?alt=media&token=a96751db-ce38-4041-bcc2-810db7c32117'
+        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fpriscilla.png?alt=media&token=33ccb3d5-9b0f-4e16-bb00-8830d558b32a'
     },
-    //  {
-    //     name: '<h2>D&aacute;niel Bozzai</h2>',
-    //     role: 'Graphic design',
+     {
+        name: '<h2>D&aacute;niel Bozzai</h2>',
+        role: 'Graphic design',
         // tslint:disable-next-line:max-line-length
-    //     img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fdaniel.png?alt=media&token=50b34431-6e2e-4e28-846e-4c33937c3a44'
-    // },
+        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fdaniel.png?alt=media&token=2d050ff0-3262-4c18-92a1-231a821d5b87'
+    },
      {
         name: '<h2>Diego Mosquera</h2>',
         role: 'Web development',
         // tslint:disable-next-line:max-line-length
-        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fdiego.png?alt=media&token=06f86c6d-4a33-4efc-8e63-964c2cecdc02'
+        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fdiego.png?alt=media&token=bbc2fd81-90c1-45ef-8f86-5cd6b4022b59'
     },
      {
         name: '<h2>Manuel F Contreras</h2>',
         role: 'Artistic producer',
         // tslint:disable-next-line:max-line-length
-        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fmanuel.png?alt=media&token=da8c7fe1-4259-4b8e-b00d-42c4fe8e6532'
+        img: 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/team%2Fmanuel.png?alt=media&token=e30a984a-ac34-4d69-8465-1d17faae99e4'
     }
 ];

0d3a5b6264ec32873be0cb9f6e4f922cfeb7a424
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2017-10-14 19:16:55 +0200

    programme update

--- src/app/programme/programme.component.ts
+++ src/app/programme/programme.component.ts
@@ -15,7 +15,7 @@ export class ProgrammeComponent implements OnInit {
 
   download() {
     // tslint:disable-next-line:max-line-length
-    const url = 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/CERVANTES_VECTORES_FEHER.pdf?alt=media&token=36bea49b-ded8-4f53-8c94-fa96feaebd97'
+    const url = 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/DE%CC%81LI-DOKU-Programme.pdf?alt=media&token=c011e86a-4c97-482e-a36f-602bb3975f0f'
 
     window.open(url, '_blank');
   }

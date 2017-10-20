59dc026b69e01a5ff8ab832db8ab8e9bae320853
Diego Mosquera <diego.mosquera@onefootball.com>
2017-10-20 17:31:22 +0200

    programme

--- src/app/programme/programme.component.ts
+++ src/app/programme/programme.component.ts
@@ -15,7 +15,7 @@ export class ProgrammeComponent implements OnInit {
 
   download() {
     // tslint:disable-next-line:max-line-length
-    const url = 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/DE%CC%81LI-DOKU-Programme.pdf?alt=media&token=c011e86a-4c97-482e-a36f-602bb3975f0f'
+    const url = 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/DE%CC%81LI-DOKU%20programme.pdf?alt=media&token=74e95466-4afa-4a4c-b4d9-6e979ddb6820'
 
     window.open(url, '_blank');
   }

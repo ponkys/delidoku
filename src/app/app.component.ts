3b94913d1930ed7084c2074d3989e45e6c7fe4de
Ponkys <ponkys@diego-15.local>
2018-10-02 18:59:18 +0200

    not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router, NavigationEnd } from '@angular/router';
 
+import {TranslateService} from '@ngx-translate/core';
 
 import { WindowRefService } from './window-ref.service';
 import { TranslationService } from './translation.service';
@@ -19,12 +20,14 @@ export class AppComponent implements OnInit {
     removeOnWindowEvent = false
 
     constructor(
+        private translate: TranslateService,
         private winRef: WindowRefService,
         private location: Location,
         private router: Router,
         private translationService: TranslationService
     ) {
-        this.translationService.init();
+        this.translate.setDefaultLang('en');
+        this.translate.use('en');
         this.changePos = this.winRef.nativeWindow.innerHeight;
     }
 

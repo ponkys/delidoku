0b129eebb021640445d539628e53b936a592ad3b
Ponkys <ponkys@diego-15.local>
2018-10-05 16:32:51 +0200

    add checking for production in analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Router, NavigationEnd } from '@angular/router';
 
 import { WindowRefService } from './window-ref.service';
 import { TranslationService } from './translation.service';
+import { environment } from 'environments/environment.prod';
 
 @Component({
     selector: 'app-root',
@@ -27,7 +28,7 @@ export class AppComponent implements OnInit {
         this.translationService.init();
         this.changePos = this.winRef.nativeWindow.innerHeight;
         this.router.events.subscribe(event => {
-            if (event instanceof NavigationEnd) {
+            if (event instanceof NavigationEnd && environment.production) {
                 (<any>window).ga('set', 'page', event.urlAfterRedirects);
                 (<any>window).ga('send', 'pageview');
             }

5a6992f0d36ac44758130e02b15be0c87679b10f
Ponkys <ponkys@diego-15.local>
2018-10-15 18:41:31 +0200

    add programme

--- src/app/programme/programme.component.ts
+++ src/app/programme/programme.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-programme',
@@ -7,16 +8,18 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./programme.component.css']
 })
 export class ProgrammeComponent implements OnInit {
+  // tslint:disable-next-line:max-line-length
+  urlEn = 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/programme%2Fprogramme_en.png?alt=media&token=9bd3b18b-0ad9-4dc0-833b-849bb7061cd0';
+  // tslint:disable-next-line:max-line-length
+  urlHu = 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/programme%2Fprogramme_hu.png?alt=media&token=34982b36-c83c-4230-9bf1-289ad095df3c';
 
-  constructor(private http: HttpClient) { }
+  constructor(private translate: TranslateService) { }
 
   ngOnInit() {
   }
 
   download() {
-    // tslint:disable-next-line:max-line-length
-    const url = 'https://firebasestorage.googleapis.com/v0/b/delidoku-b2ece.appspot.com/o/Deli_Doku_Programme.pdf?alt=media&token=7cd854c5-85ab-4604-9416-f98884ed0ca4'
-
+    const url = this.translate.currentLang === 'en' ? this.urlEn : this.urlHu;
     window.open(url, '_blank');
   }
 

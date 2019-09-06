ccc4a39a64323830155fa248616f73c436cc808d
ponkys <diego.mosquera.soto@gmail.com>
2019-09-06 15:33:29 +0200

    [T] Add 2019 films

---
+++ src/app/film-2019/film-2019.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Film2018 } from '../films/film';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { environment } from 'environments/environment';
+import { FILMS_2019, FILMS_2019_HU } from '../films/films-2019';
+import { Subscription } from 'rxjs';
+
+const BLANK_CATEGORIES = [
+  {
+    name: 'FILMS.OFFICIAL_SELECTION',
+    films: []
+  },
+  {
+    name: 'Focus Argentina',
+    films: []
+  }
+];
+
+interface Category {
+  name: string;
+  films: Film2018[];
+}
+
+@Component({
+  selector: 'app-film-2019',
+  templateUrl: './film-2019.component.html',
+  styleUrls: ['../film-2017/film-2017.component.css']
+})
+export class Film2019Component implements OnInit, OnDestroy {
+  title = 'FILMS.TITLE';
+  categories: Array<Category> = [];
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(private tr: TranslateService) {}
+
+  ngOnInit() {
+    this.onLanguageChange();
+    if (this.categories.length === 0) {
+      this.determineFilmsBasedOnLanguage(this.tr.currentLang);
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
+
+  private mapFilms(films: Film2018[], categories: Category[]): Category[] {
+    return categories.map((sectionObj: Category, i: number) => {
+      // reset array
+      sectionObj.films = [];
+      films.map(filmObj => {
+        if (filmObj.section === sectionObj.name) {
+          categories[i].films = [...categories[i].films, filmObj];
+        }
+      });
+      return sectionObj;
+    });
+  }
+
+  private onLanguageChange() {
+    this.subscriptions.push(
+      this.tr.onLangChange.subscribe((event: LangChangeEvent) => {
+        this.determineFilmsBasedOnLanguage(event.lang);
+      })
+    );
+  }
+
+  private determineFilmsBasedOnLanguage(lang: string): void {
+    if (lang === environment.LANGUAGES.EN) {
+      this.categories = this.mapFilms(FILMS_2019, [...BLANK_CATEGORIES]);
+      return;
+    }
+    this.categories = this.mapFilms(FILMS_2019_HU, [...BLANK_CATEGORIES]);
+  }
+}
---
+++ src/app/films/films-2019.ts
@@ -0,0 +1,191 @@
+import { Film2018 } from './film';
+
+export const FILMS_2019: Film2018[] = [
+  {
+    title: `A girl's band`,
+    // tslint:disable-next-line:max-line-length
+    synopsis: '',
+    trailer:
+      'https://player.vimeo.com/video/299064472?color=ffffff&title=0&byline=0&portrait=0',
+    duration: 83,
+    year: 2018,
+    author: 'Marilina Giménez',
+    country: 'Argentina',
+    date: [],
+    section: 'Focus Argentina',
+    subtitleNote: ''
+  },
+  {
+    title: 'Silvia',
+    trailer:
+      'https://player.vimeo.com/video/358303061?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 103,
+    year: 2018,
+    author: 'María Silvia Esteve',
+    country: 'Argentina, Chile',
+    date: [],
+    section: 'Focus Argentina',
+    subtitleNote: ''
+  },
+  {
+    title: 'Us women, them women',
+    trailer:
+      'https://player.vimeo.com/video/358302844?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 63,
+    year: 2016,
+    author: 'Julia Pesce',
+    country: 'Argentina',
+    date: [],
+    section: 'Focus Argentina',
+    subtitleNote: ''
+  },
+  {
+    title: 'The Smiling Lombana',
+    trailer:
+      'https://player.vimeo.com/video/358303791?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 87,
+    year: 2019,
+    author: 'Daniela Abad',
+    country: 'Colombia',
+    date: [],
+    section: 'FILMS.OFFICIAL_SELECTION',
+    subtitleNote: ''
+  },
+  {
+    title: 'My Aunt Toty',
+    trailer:
+      'https://player.vimeo.com/video/358302601?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 87,
+    year: 2017,
+    author: 'Leon Felipe Troya',
+    country: 'Ecuador',
+    date: [],
+    section: 'FILMS.OFFICIAL_SELECTION',
+    subtitleNote: ''
+  },
+  {
+    title: 'Baronesa',
+    trailer:
+      'https://player.vimeo.com/video/358303962?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 71,
+    year: 2017,
+    author: 'Juliana Antunes',
+    country: 'Brazil',
+    date: [],
+    section: 'FILMS.OFFICIAL_SELECTION',
+    subtitleNote: ''
+  },
+  {
+    title: 'Beauties of the Night',
+    trailer:
+      'https://player.vimeo.com/video/358302180?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 92,
+    year: 2016,
+    author: 'María José Cuevas',
+    country: 'Mexico',
+    date: [],
+    section: 'FILMS.OFFICIAL_SELECTION',
+    subtitleNote: ''
+  }
+];
+
+export const FILMS_2019_HU: Film2018[] = [
+  {
+    title: `A girl's band`,
+    // tslint:disable-next-line:max-line-length
+    synopsis: '',
+    trailer:
+      'https://player.vimeo.com/video/299064472?color=ffffff&title=0&byline=0&portrait=0',
+    duration: 83,
+    year: 2018,
+    author: 'Marilina Giménez',
+    country: 'Argentina',
+    date: [],
+    section: 'Focus Argentina',
+    subtitleNote: ''
+  },
+  {
+    title: 'Silvia',
+    trailer:
+      'https://player.vimeo.com/video/358303061?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 103,
+    year: 2018,
+    author: 'María Silvia Esteve',
+    country: 'Argentina, Chile',
+    date: [],
+    section: 'Focus Argentina',
+    subtitleNote: ''
+  },
+  {
+    title: 'Us women, them women',
+    trailer:
+      'https://player.vimeo.com/video/358302844?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 63,
+    year: 2016,
+    author: 'Julia Pesce',
+    country: 'Argentina',
+    date: [],
+    section: 'Focus Argentina',
+    subtitleNote: ''
+  },
+  {
+    title: 'The Smiling Lombana',
+    trailer:
+      'https://player.vimeo.com/video/358303791?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 87,
+    year: 2019,
+    author: 'Daniela Abad',
+    country: 'Colombia',
+    date: [],
+    section: 'FILMS.OFFICIAL_SELECTION',
+    subtitleNote: ''
+  },
+  {
+    title: 'My Aunt Toty',
+    trailer:
+      'https://player.vimeo.com/video/358302601?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 87,
+    year: 2017,
+    author: 'Leon Felipe Troya',
+    country: 'Ecuador',
+    date: [],
+    section: 'FILMS.OFFICIAL_SELECTION',
+    subtitleNote: ''
+  },
+  {
+    title: 'Baronesa',
+    trailer:
+      'https://player.vimeo.com/video/358303962?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 71,
+    year: 2017,
+    author: 'Juliana Antunes',
+    country: 'Brazil',
+    date: [],
+    section: 'FILMS.OFFICIAL_SELECTION',
+    subtitleNote: ''
+  },
+  {
+    title: 'Beauties of the Night',
+    trailer:
+      'https://player.vimeo.com/video/358302180?color=ffffff&title=0&byline=0&portrait=0',
+    synopsis: '',
+    duration: 92,
+    year: 2016,
+    author: 'María José Cuevas',
+    country: 'Mexico',
+    date: [],
+    section: 'FILMS.OFFICIAL_SELECTION',
+    subtitleNote: ''
+  }
+];

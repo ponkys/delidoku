c5b5083ea930fa7a2b6cf3281047b3369ab733ab
ponkys <diego.mosquera.soto@gmail.com>
2019-09-06 14:53:36 +0200

    [T] Comment home buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,46 +1,42 @@
 import { Component } from '@angular/core';
 import { WindowRefService } from '../window-ref.service';
 import { PARTICLES_PARAMS } from './particles/particles-params';
-import { TRAILERS } from './trailers-const';
 import { PARTICLES_STYLES } from './particles/particles-styles';
-import { TranslateService } from '@ngx-translate/core';
+import { TRAILERS } from './trailers-const';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-    currentVideo: string;
-    height = 100;
-    isHomeComponent: false;
-    mobileScreen = true;
-    myParams: object = PARTICLES_PARAMS;
-    particlesStyle: object = PARTICLES_STYLES;
-    showVideo: boolean;
-    trialers = TRAILERS;
-    width = 100;
+  currentVideo: string;
+  height = 100;
+  isHomeComponent: false;
+  mobileScreen = true;
+  myParams: object = PARTICLES_PARAMS;
+  particlesStyle: object = PARTICLES_STYLES;
+  showVideo: boolean;
+  trailers = TRAILERS;
+  width = 100;
 
-    constructor(
-        private winRef: WindowRefService
-    ) {
-        if (this.winRef.nativeWindow.innerWidth > 768) {
-            this.mobileScreen = false;
-        }
+  constructor(private winRef: WindowRefService) {
+    if (this.winRef.nativeWindow.innerWidth > 768) {
+      this.mobileScreen = false;
     }
+  }
 
-    onClickVideo(video: string) {
-        this.currentVideo = video;
-        this.showVideo = true;
-    }
+  onClickVideo(video: string) {
+    this.currentVideo = video;
+    this.showVideo = true;
+  }
 
-    onCloseVideo() {
-        this.showVideo = false;
-        this.currentVideo = null;
-    }
+  onCloseVideo() {
+    this.showVideo = false;
+    this.currentVideo = null;
+  }
 
-    facebookNavigate() {
-        window.open('https://www.facebook.com/delidokubp/', '_blank');
-    }
-
+  facebookNavigate() {
+    window.open('https://www.facebook.com/delidokubp/', '_blank');
+  }
 }
